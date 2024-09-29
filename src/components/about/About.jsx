5667fe108f484660cf068bc0420d9f01d9721c34
Saceanu Ionut <saceanu.ionut@icloud.com>
2024-09-29 13:04:02 +0300

    add

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,29 +7,24 @@ const About = () => {
   return (
     <div className='about'>
       <div className='aboutUs'>
-        <img src={contact2} alt="contact2" />
+        <img src={contact2} alt='contact2' />
         <img src={contact} alt='contactUs' />
-        <img src={contact3} alt="contact4" />
-        <h1>About Us</h1>
-        <p>
-          At [Your Company], we are passionate about creating unique digital
-          experiences, combining creative design with innovative web development
-          solutions. With a dedicated team of specialists in UI/UX design and
-          web programming, we focus on developing functional, attractive, and
-          optimized websites for any device.
-        </p>
-        <p>
-          Our mission is to transform your vision into a memorable online
-          presence, ensuring that each project is tailored to reflect your
-          brand's identity. We use the latest technologies and industry trends
-          to create fast, secure, and user-friendly platforms.
-        </p>
-        <p>
-          Whether you need a presentation website, an online store, or a complex
-          web application, we are here to bring your ideas to life and ensure
-          your success in the digital space.
-        </p>
+        <img src={contact3} alt='contact4' />
       </div>
+      <h1>About Us</h1>
+      <p>
+        At [Your Company], we are passionate about creating unique digital
+        experiences, combining creative design with innovative web development
+        solutions. With a dedicated team of specialists in UI/UX design and web
+        programming, we focus on developing functional, attractive, and
+        optimized websites for any device. Our mission is to transform your
+        vision into a memorable online presence, ensuring that each project is
+        tailored to reflect your brand's identity. We use the latest
+        technologies and industry trends to create fast, secure, and
+        user-friendly platforms. Whether you need a presentation website, an
+        online store, or a complex web application, we are here to bring your
+        ideas to life and ensure your success in the digital space.
+      </p>
     </div>
   );
 };
